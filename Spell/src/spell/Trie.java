9c6c5e1ebc7c92354008dd17da4ea89284e5f64e
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-09-12 21:44:58 -0600

    Finished Trie Class

--- Spell/src/spell/Trie.java
+++ Spell/src/spell/Trie.java
@@ -3,6 +3,8 @@ package spell;
 public class Trie implements ITrie{
 
     private INode root = new Node();
+    private int nodeCount = 1;
+    private int wordCount = 0;
 
     @Override
     public void add(String word) {
@@ -14,7 +16,10 @@ public class Trie implements ITrie{
         if(temp == null){
             char letter = word.charAt(0);
             int index = getIndex(letter);
-            root.getChildren()[index] = new Node();
+            if (root.getChildren()[index] == null) {
+                root.getChildren()[index] = new Node();
+                nodeCount++;
+            }
             temp = root.getChildren()[index];
             INode temp2;
             for(int i=1; i<word.length(); i++){
@@ -23,6 +28,7 @@ public class Trie implements ITrie{
                 temp2 = step(temp, letter);
                 if(temp2 == null){
                     temp.getChildren()[index] = new Node();
+                    nodeCount++;
                     temp = temp.getChildren()[index];
                 }
                 else{
@@ -32,6 +38,8 @@ public class Trie implements ITrie{
         }
         // increase counter of the word
         temp.incrementValue();
+        if(temp.getValue() == 1)
+            wordCount++;
     }
 
     public int getIndex(char letter){
@@ -57,7 +65,6 @@ public class Trie implements ITrie{
         char letter = word.charAt(0);
         // step through Trie and try to find words
         INode temp = step(root, letter);
-
         for(int i=1; i<word.length(); i++){
             // if we ever reach null, the tree is over and the word isn't there
             if(temp == null)
@@ -70,7 +77,7 @@ public class Trie implements ITrie{
                 return null;
         }
         // if the value is 0, the word isn't really there
-        if(temp.getValue() == 0)
+        if(temp == null || temp.getValue() == 0)
             return null;
         // return found Trie
         return temp;
@@ -78,56 +85,108 @@ public class Trie implements ITrie{
 
     @Override
     public int getWordCount() {
-        return wordCountHelper(root);
-    }
-
-    public int wordCountHelper(INode rootNode){
-        // create and set variables
-        boolean childPresent = false;
-        INode child;
-
-        // ----- BASE CASE 1 -----
-        //  if we hit an empty node, return 0
-        if(rootNode == null)
-            return 0;
-
-        // ----- BASE CASE 2 -----
-        // check if root node has any children
-        for(int i=0; i<26; i++){
-            child = rootNode.getChildren()[i];
-            if(child != null){
-                childPresent = true;
-                break;
-            }
-        }
-        // if there is not a child present and if there is a word at the node, return 1 (it's a leaf)
-        // if there is not a word at the node, return a 0 (it's an invalid word)
-        if(!childPresent){
-            if(rootNode.getValue() != 0)
-                return 1;
-            else
-                return 0;
-        }
-
-        // ----- RECURSIVE CASE -----
-
+        return wordCount;
     }
 
     @Override
     public int getNodeCount() {
-        return nodeCountHelp(root);
+        return nodeCount;
     }
 
-    public int nodeCountHelp(INode rootNode){
-        if(rootNode == null)
-            return 0;
+    @Override
+    public String toString() {
+        StringBuilder curWord = new StringBuilder();
+        StringBuilder output = new StringBuilder();
+        stringHelper(root, curWord, output);
+        return output.toString();
+    }
 
-        int sum = 0;
-        for(int i=0; i<26; i++){
-            sum += nodeCountHelp(rootNode.getChildren()[i]);
+    private void stringHelper(INode rootNode, StringBuilder curWord, StringBuilder output){
+        INode child;
+        char childLetter;
+
+        if(rootNode.getValue() > 0){
+            // Append nodes word to output
+            output.append(curWord.toString());
+            output.append("\n");
         }
-        if(sum != 0)
-            return 1 + sum;
-        return 1;
+
+        // look at all the children
+        for(int i=0; i<26; i++){
+            child = rootNode.getChildren()[i];
+            if(child != null) {
+                childLetter = (char) ('a' + i);
+                curWord.append(childLetter);
+                stringHelper(child, curWord, output);
+                curWord.deleteCharAt(curWord.length() - 1);
+            }
+        }
+    }
+
+
+    @Override
+    public int hashCode() {
+        // values to consider
+        // word count, node count, index of the root node's non-null children
+        int sumInd = 0;
+        for(int i=0; i<26; i++){
+            if(root.getChildren()[i] != null)
+                    sumInd += i;
+        }
+        // return unique combination of values
+        return (3*wordCount + 5*nodeCount + 7*sumInd);
+    }
+
+    @Override
+    public boolean equals(Object o){
+        // check if o is null, or doesn't have the same class ----- check if o is the exact same object
+        if(o == null || o.getClass() != this.getClass())
+            return false;
+        if(o == this)
+            return true;
+
+        Trie dict = (Trie)o;
+
+        int oWord = dict.getWordCount();
+        int thisWord = this.getWordCount();
+        int oNode = dict.getNodeCount();
+        int thisNode= this.getNodeCount();
+
+        if(oWord != thisWord || oNode != thisNode)
+            return false;
+
+        return eqHelper(this.root, dict.root);
+    }
+
+    private boolean eqHelper(INode n1, INode n2){
+        // compare n1 and n2
+            // are the counts the same
+            // do the nodes have non-null children in the same location
+        if(n1.getValue() != n2.getValue())
+            return false;
+        for(int i=0; i<26; i++){
+            INode child1 = n1.getChildren()[i];
+            INode child2 = n2.getChildren()[i];
+
+            if(child1 == null){
+                if(child2 != null)
+                    return false;
+            }else if(child2 == null) {
+                return false;
+            }
+        }
+
+        // recurse on children and compare the child subtrees
+        boolean finalTest = true;
+        for(int i=0; i<26; i++){
+            INode child1 = n1.getChildren()[i];
+            INode child2 = n2.getChildren()[i];
+            if(child1 != null)
+                finalTest = eqHelper(child1, child2);
+            if(!finalTest)
+                break;
+        }
+        return finalTest;
     }
 }
+
