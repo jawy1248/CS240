17e29544856c27f1f94c9e374589165764f3522e
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-09-13 14:55:48 -0600

    Moved files to correct location

--- Spell/src/spell/SpellCorrector.java
+++ Spell/src/spell/SpellCorrector.java
@@ -5,7 +5,13 @@ import java.io.IOException;
 public class SpellCorrector implements ISpellCorrector{
     @Override
     public void useDictionary(String dictionaryFileName) throws IOException {
-
+//        if(dictionaryFileName.length() != 0){
+//            var file = new File(dictionaryFileName);
+//            if(file.exists()){
+//                var scanner = new Scanner(file);
+//
+//            }
+//        }
     }
 
     @Override
