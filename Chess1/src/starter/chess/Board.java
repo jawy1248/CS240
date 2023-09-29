286a735b40e9797548b7114ecefbc9035d4006ec
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-09-29 17:20:17 -0600

    update(board) finished the board (except resetting board)

--- Chess1/src/starter/chess/Board.java
+++ Chess1/src/starter/chess/Board.java
@@ -2,18 +2,30 @@ package chess;
 
 public class Board implements ChessBoard{
 
+    // Always use [row][col]
+    private ChessPiece[][] board = new ChessPiece[8][8];
+
     @Override
     public void addPiece(ChessPosition position, ChessPiece piece) {
+        int row = position.getRow();
+        int col = position.getColumn();
 
+        board[row][col] = piece;
     }
 
     @Override
     public ChessPiece getPiece(ChessPosition position) {
-        return null;
+        int row = position.getRow();
+        int col = position.getColumn();
+
+        return board[row][col];
     }
 
     @Override
     public void resetBoard() {
-
+        for(int i=0; i<8; i++){
+            for(int j=0; j<8; j++)
+                board[i][j] = null;
+        }
     }
 }
--- Chess1/src/starter/chess/Position.java
+++ Chess1/src/starter/chess/Position.java
@@ -6,7 +6,7 @@ public class Position implements ChessPosition {
     private int col;
 
 
-    public void setPosition(ChessPiece piece, int row, int col) {
+    public void setPosition(int row, int col) {
         this.row = row;
         this.col = col;
     }
