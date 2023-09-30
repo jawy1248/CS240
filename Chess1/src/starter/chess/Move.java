0b41ced3c4921bf731f00fa77026b7e476f6705b
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-09-29 22:33:30 -0600

    updated(chess1)

--- Chess1/src/starter/chess/Move.java
+++ Chess1/src/starter/chess/Move.java
@@ -27,4 +27,14 @@ public class Move implements ChessMove {
     public ChessPiece.PieceType getPromotionPiece() {
         return promo;
     }
+
+    public boolean checkMove(ChessBoard board, ChessGame.TeamColor color, ChessPosition newPos){
+        ChessPiece newSpot = board.getPiece(newPos);
+        if(newSpot != null) {
+            if(newSpot.getTeamColor() == color)
+                return false;
+        }
+
+        return false;
+    }
 }
--- Chess1/src/starter/chess/Piece.java
+++ Chess1/src/starter/chess/Piece.java
@@ -1,10 +1,10 @@
 package chess;
 
 import java.util.Collection;
+import java.util.Set;
 
 public class Piece implements ChessPiece {
 
-    // 0 is white, 1 is black
     private ChessGame.TeamColor color;
     private PieceType type;
 
@@ -25,6 +25,42 @@ public class Piece implements ChessPiece {
 
     @Override
     public Collection<ChessMove> pieceMoves(ChessBoard board, ChessPosition myPosition) {
+        ChessPiece piece = board.getPiece(myPosition);
+        if(piece == null)
+            return null;
+
+        Collection<ChessPosition> possMoves;
+
+        int row = myPosition.getRow();
+        int col = myPosition.getColumn();
+
+        PieceType tempType = piece.getPieceType();
+        ChessGame.TeamColor tempColor = piece.getTeamColor();
+
+        switch (tempType) {
+            case KING -> {
+                for(int i=-1; i<2; i++){
+                    int tempRow = row+i;
+                    if(tempRow < 8 && tempRow > 0) {}
+//                        possMoves.add(new Position(tempRow, col));
+                }
+            }
+            case QUEEN -> {
+
+            }
+            case ROOK -> {
+
+            }
+            case KNIGHT -> {
+
+            }
+            case BISHOP -> {
+
+            }
+            case PAWN -> {
+
+            }
+        }
         return null;
     }
 }
