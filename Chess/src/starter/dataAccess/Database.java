41c34febfb9907854dbe85f338aa9ca302c7dbe5
jawy1248 <112504611+jawy1248@users.noreply.github.com>
2023-11-06 19:06:57 -0700

    Added starter code

--- Chess/src/starter/dataAccess/Database.java
+++ Chess/src/starter/dataAccess/Database.java
@@ -1,151 +1,68 @@
 package dataAccess;
 
-import chess.ChessGame;
-import java.util.HashSet;
-import java.util.UUID;
-import java.util.Set;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.LinkedList;
 
 /**
- * Acts as an in-memory database before running SQL
+ * Database is responsible for creating connections to the database. Connections are
+ * managed with a simple pool in order to increase performance. To obtain and
+ * use connections represented by this class use the following pattern.
+ *
+ * <pre>
+ *  public boolean example(String selectStatement, Database db) throws DataAccessException{
+ *    var conn = db.getConnection();
+ *    try (var preparedStatement = conn.prepareStatement(selectStatement)) {
+ *        return preparedStatement.execute();
+ *    } catch (SQLException ex) {
+ *        throw new DataAccessException(ex.toString());
+ *    } finally {
+ *        db.returnConnection(conn);
+ *    }
+ *  }
+ * </pre>
  */
 public class Database {
 
-    /**
-     * Private variables include the databases for the user, auth, and game
-     */
-    private static User_DAO userDB = new User_DAO();
-    private static Auth_DAO authDB = new Auth_DAO();
-    private static Game_DAO gameDB = new Game_DAO();
+    // FIXME: Change these fields, if necessary, to match your database configuration
+    public static final String DB_NAME = "chess";
+    private static final String DB_USERNAME = "root";
+    private static final String DB_PASSWORD = "Miller12";
+
+    private static final String CONNECTION_URL = "jdbc:mysql://localhost:3306";
+
+    private final LinkedList<Connection> connections = new LinkedList<>();
 
     /**
-     * Clears all the databases...complete clear
+     * Get a connection to the database. This pulls a connection out of a simple
+     * pool implementation. The connection must be returned to the pool after
+     * you are done with it by calling {@link #returnConnection(Connection) returnConnection}.
+     *
+     * @return Connection
      */
-    public void clearAll(){
-        userDB.clearAllUsers();
-        authDB.clearAllAuth();
-        gameDB.clearAllGames();
-    }
-
-    // -------------------- USERS --------------------
-    /**
-     * Finds a User in the userDB. Returns the whole record of the user
-     * @param username String of the username to look for
-     * @return User_Record object of the found user (or null if none are found)
-     */
-    public User_Record findUser(String username){
-        return userDB.getUserData(username);
-    }
-
-    /**
-     * Adds a user to the userDB
-     * @param user User_Record object to be added to the DB
-     */
-    public void addUser(User_Record user){
-        userDB.addUser(user);
-    }
-
-    // -------------------- AUTHS --------------------
-    /**
-     * Creates a random AuthToken
-     * @return String of AuthToken
-     */
-    public String createAuthToken(){
-        return UUID.randomUUID().toString();
-    }
-
-    /**
-     * Adds an AuthToken to the DB
-     * @param auth Auth_Record object to be added
-     */
-    public void addAuthToken(Auth_Record auth) { authDB.addAuth(auth); }
-
-    /**
-     * Removes an AuthToken from the DB
-     * @param username String of the username to be removed
-     */
-    public void removeAuthToken(String username) { authDB.removeAuth(username); }
-
-    /**
-     * Finds a username for a specific AuthToken
-     * @param authToken String of the AuthToken
-     * @return String of the associated username
-     */
-    public String getUsername(String authToken) { return authDB.getUsername(authToken); }
-
-    /**
-     * Searches to see if an AuthToken exists
-     * @param authToken String of AuthToken to be found
-     * @return true if found, false otherwise
-     */
-    public boolean findAuthToken(String authToken) { return authDB.findAuthToken(authToken); }
-
-    // -------------------- GAMES --------------------
-    /**
-     * Randomly creates a gameID
-     * @return int of gameID
-     */
-    public int createGameID(){ return (int)(Math.random() * 10000);}
-
-    /**
-     * Adds a game to the DB
-     * @param game Game_Record object to be added
-     */
-    public void createGame(Game_Record game) { gameDB.addGame(game); }
-
-    /**
-     * Searches if a gameID is a valid current ID
-     * @param gameID int of gameID to be found
-     * @return true if found, false otherwise
-     */
-    public boolean findGameID(int gameID){ return gameDB.findGameID(gameID); }
-
-    /**
-     * Adds a user to a game already created as either white or black
-     * @param color BLACK/WHITE to say request color
-     * @param gameID int of gameID to be added to
-     * @param username String of username to be added
-     */
-    public void joinGame(ChessGame.TeamColor color, int gameID, String username){ gameDB.joinGame(color, gameID, username); }
-
-    /**
-     * Adds a user to a game already created as an observer
-     * @param gameID int of gameID to be added to
-     * @param username String of username to be added
-     */
-    public void observeGame(int gameID, String username){ gameDB.addObserver(gameID, username);}
-
-    /**
-     * Searches if a game has a specific color taken
-     * @param color BLACK/WHITE, the color to check if it is taken
-     * @param gameID String of the gameID to look at
-     * @return true if found, false otherwise
-     */
-    public boolean findGameColor(ChessGame.TeamColor color, int gameID){ return gameDB.findGameColor(color, gameID); }
-
-    /**
-     * Lists all the games as a HashSet of Game_Records
-     * @return HashSet of Game_Records currently in use
-     */
-    public HashSet<Game_Record> listGames(){
-        HashSet<Game_Record> listOfGames = new HashSet<>();
-        // Iterate through each of the gameIDs and add each game to the set
-        Set<Integer> keys = gameDB.getKeys();
-        for (Integer key : keys) {
-            // Temps of each of the objects
-            int temp = key;
-            String tempWhite = gameDB.getWhiteUsername(temp);
-            String tempBlack = gameDB.getBlackUsername(temp);
-            String tempName = gameDB.getGameName(temp);
-
-            // Create and add temp Game_Record
-            Game_Record tempRecord = new Game_Record(temp, tempWhite, tempBlack, tempName, null);
-            listOfGames.add(tempRecord);
+    synchronized public Connection getConnection() throws DataAccessException {
+        try {
+            Connection connection;
+            if (connections.isEmpty()) {
+                connection = DriverManager.getConnection(CONNECTION_URL, DB_USERNAME, DB_PASSWORD);
+                connection.setCatalog(DB_NAME);
+            } else {
+                connection = connections.removeFirst();
+            }
+            return connection;
+        } catch (SQLException e) {
+            throw new DataAccessException(e.getMessage());
         }
-        // Return the list
-        return listOfGames;
     }
 
-    public User_DAO getUserDB(){ return userDB; }
-    public Auth_DAO getAuthDB(){ return authDB; }
-    public Game_DAO getGameDB(){ return gameDB; }
+    /**
+     * Return a previously acquired connection to the pool.
+     *
+     * @param connection previous obtained by calling {@link #getConnection() getConnection}.
+     */
+    synchronized public void returnConnection(Connection connection) {
+        connections.add(connection);
+    }
 }
+
